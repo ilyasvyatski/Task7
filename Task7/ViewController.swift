a6e5c22ff4f9c106d9ced5bbe547a8b5965b0a90
Alex <63662620+AlexFoxD@users.noreply.github.com>
2022-09-07 16:08:44 +0300

    Initial Commit

---
+++ Task7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task7
+//
+//  Created by neoviso on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
